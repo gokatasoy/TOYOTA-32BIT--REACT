4b793648506f2b725edda66424bda799219a2276
Gökberk ATASOY <gokatasoycs@gmail.com>
2023-04-01 12:06:48 +0300

    Add NrReason component to ErrorList

---
+++ toyota-32bit/src/components/nr-reason/NrReason.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "./nrReason.css"
+
+function NrReason() {
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  return (
+    <select value={selectedOption} onChange={handleChange}>
+      <option value="">Select an option</option>
+      <option value="option1">Option 1</option>
+      <option value="option2">Option 2</option>
+      <option value="option3">Option 3</option>
+      <option value="option4">Option 4</option>
+      <option value="option5">Option 5</option>
+    </select>
+  );
+}
+
+export default NrReason;
