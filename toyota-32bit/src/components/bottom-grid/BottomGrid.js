49dd919aad34d7e4779f1201a2ad94f2b48a2ff1
Gökberk ATASOY <gokatasoycs@gmail.com>
2023-04-01 12:06:26 +0300

    Add BottomGrid component to ErrorList

---
+++ toyota-32bit/src/components/bottom-grid/BottomGrid.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./bottomGrid.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
+function BottomGrid() {
+    return (
+        <div className='BottomGrid'>
+            <div className='montaj-body'>
+                <div className='section'>
+                    <label>MONTAJ NO:</label>
+                    <input/>
+                    <span className='search-button'>ARA</span>
+                    <FontAwesomeIcon className='updown-button' icon={faChevronUp} />
+                </div>
+                <div className='section'>
+                    <label>BODY NO:</label>
+                    <input/>
+                    <span className='search-button'>ARA</span>
+                    <FontAwesomeIcon className='updown-button' icon={faChevronDown} />
+                </div>
+            </div>
+            <div className='car-list'>ARAÇ LİSTESİ</div>
+            <div className='manuel-error'>MANUEL HATA</div>
+            <div className='multiple-error'>ÇOKLU HATA</div>
+            <div className='error-list'>HATA LİSTESİ</div>
+            <div className='error-copy'>HATA KOPYA</div>
+            <div className='exit'>ÇIKIŞ YAP</div>
+        </div>
+    )
+}
+
+export default BottomGrid
