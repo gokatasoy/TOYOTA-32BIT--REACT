93afde8da99e2b2a9ef0c4dd988c0ba8140a0f6b
Gökberk ATASOY <gokatasoycs@gmail.com>
2023-05-02 11:16:14 +0300

    Add Box Datas (Box Data V1)

--- toyota-32bit/src/pages/DefectPage/DefectPage.js
+++ toyota-32bit/src/pages/DefectPage/DefectPage.js
@@ -1,57 +1,114 @@
-import React from 'react'
-import "./defectPage.css"
-import DefectPageHeader from '../../components/defect-page-header/DefectPageHeader'
-import dummyCar from "../../dummy-car.jpeg" 
-import { useNavigate } from 'react-router-dom'
-import AssyNo from '../../components/assy-no/AssyNo'
+import React from "react";
+import "./defectPage.css";
+import DefectPageHeader from "../../components/defect-page-header/DefectPageHeader";
+import dummyCar from "../../dummy-car.jpeg";
+import { useNavigate } from "react-router-dom";
+import AssyNo from "../../components/assy-no/AssyNo";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function DefectPage() {
-    let navigate = useNavigate()
+    let navigate = useNavigate();
+
+    const [boxesData, setBoxesData] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("./picture-data.json")
+            .then((res) => setBoxesData(res.data))
+            .catch((error) => {
+                console.log(error);
+                setBoxesData("An error occured while fetching data.");
+            });
+    }, []);
 
     const handleCloseClick = () => {
-        navigate("/")
-    }
+        navigate("/");
+    };
 
     const handleBackClick = () => {
-        navigate("/terminal-login")
-    }
+        navigate("/terminal-login");
+    };
 
     const handleDefectListClick = () => {
-        navigate("/defect-list")
-    }
+        navigate("/defect-list");
+    };
 
     const handleBigFontClick = () => {
-        navigate("/big-font")
-    }
+        navigate("/big-font");
+    };
 
     return (
-        <div className='defect-page-grid'>
-            <div className='defect-page-container'>
-                <div className='header-container'>
-                    <DefectPageHeader/>
+        <div className="defect-page-grid">
+            <div className="defect-page-container">
+                <div className="header-container">
+                    <DefectPageHeader />
                 </div>
-                <div className='row'>                  
-                    <div className='image-container'>
-                        <img src={dummyCar} alt="dummy-car"/>
-                        <div className='button-container button-container-bottom'>
-                            <div className='button' onClick={handleCloseClick}>ÇIKIŞ</div>
-                            <div className='button' onClick={handleBackClick}>GERİ</div>
-                            <div className='button'>MODEL İLK RESMİ</div>
-                            <div className='button' onClick={handleDefectListClick}>HATA LİSTESİ</div>
-                            <div className='button'>TEMİZLE</div>
-                            <div className='button' onClick={handleBigFontClick}>BÜYÜK FONT</div>
+
+                <div className="row">
+                    {/* IMAGE CONTAINER */}
+                    <div className="image-container">
+                        <img src={dummyCar} alt="dummy-car" />
+                        <div className="box-containerr">
+                            {boxesData.map(({ defectButtonRecords }) => (
+                                <div key={defectButtonRecords}>
+                                    {defectButtonRecords.map(
+                                        ({
+                                            buttonId,
+                                            boxX,
+                                            boxY,
+                                            boxWidth,
+                                            boxHeight,
+                                            boxColor,
+                                            labelColor,
+                                            labelText,
+                                        }) => (
+                                            <div
+                                                key={buttonId}
+                                                className="box"
+                                                style={{
+                                                    left: boxX + "px",
+                                                    top: boxY + "px",
+                                                    width: boxWidth + "px",
+                                                    height: boxHeight + "px",
+                                                    backgroundColor: boxColor,
+                                                    color: labelColor,
+                                                }}
+                                            >
+                                                {labelText}
+                                            </div>
+                                        )
+                                    )}
+                                </div>
+                            ))}
+                        </div>
+                        <div className="button-container button-container-bottom">
+                            <div className="button" onClick={handleCloseClick}>
+                                ÇIKIŞ
+                            </div>
+                            <div className="button" onClick={handleBackClick}>
+                                GERİ
+                            </div>
+                            <div className="button">MODEL İLK RESMİ</div>
+                            <div className="button" onClick={handleDefectListClick}>
+                                HATA LİSTESİ
+                            </div>
+                            <div className="button">TEMİZLE</div>
+                            <div className="button" onClick={handleBigFontClick}>
+                                BÜYÜK FONT
+                            </div>
                         </div>
                     </div>
 
-                    <div className='container'>
-                        <div className='checkbox-container'>
-                            <div className='checkbox'> 
+                    <div className="container">
+                        <div className="checkbox-container">
+                            <div className="checkbox">
                                 <label>
-                                    <input type="checkbox"/>
+                                    <input type="checkbox" />
                                     Harigami
                                 </label>
                             </div>
-                            <div className='checkbox'> 
+                            <div className="checkbox">
                                 <label>
                                     <input type="checkbox" />
                                     RDD
@@ -59,22 +116,24 @@ function DefectPage() {
                             </div>
                         </div>
 
-                        <div className='button-container button-container-right'>
-                            <div className='button'>HIZLI KAYDET</div>
-                            <div className='button'>KAYDET VE GEÇ</div>
-                            <div className='button'>HATA KAYIT</div>
-                            <div><AssyNo/></div>
-                            <div className='button'>TERMİNAL İLK RESMİ</div>
-                            <div className='button'>SIK GELEN HATA</div>
-                            <div className='button'>MANİFEST</div>
+                        <div className="button-container button-container-right">
+                            <div className="button">HIZLI KAYDET</div>
+                            <div className="button">KAYDET VE GEÇ</div>
+                            <div className="button">HATA KAYIT</div>
+                            <div>
+                                <AssyNo />
+                            </div>
+                            <div className="button">TERMİNAL İLK RESMİ</div>
+                            <div className="button">SIK GELEN HATA</div>
+                            <div className="button">MANİFEST</div>
                         </div>
                     </div>
 
                 </div>
+
             </div>
         </div>
-        
-    )
+    );
 }
 
-export default DefectPage
+export default DefectPage;
