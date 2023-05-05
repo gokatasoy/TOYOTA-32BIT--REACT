94cbc00eda78c6410a975a9273e7e5e496233250
Gökberk ATASOY <gokatasoycs@gmail.com>
2023-05-05 14:38:37 +0300

    Update defectEntryPopup, terminalLogin css styles

--- toyota-32bit/src/pages/TerminalLogin/TerminalLogin.js
+++ toyota-32bit/src/pages/TerminalLogin/TerminalLogin.js
@@ -33,49 +33,49 @@ function TerminalLogin() {
     return (
         <div className='login-page-grid'>
             <div className='login-page-container'>
-                <div className='terminal-name'>
-                    <h1>Terminal Name</h1>
+                <div className='row top-bar'>
+                    <div className='terminal-name'>Terminal Name</div>
                 </div>
+                <div className='row'>
+                    <div className='form-container'>
+                        <div className='form-row terminal-list'>
+                            <label>Terminal Listesi:</label>
+                                <select className='select-terminal-list'>
+                                    {veri.map(({termName}) => (
+                                    <option key={veri.termId}>{termName}</option>))}
+                                </select>
+                        </div>           
+                        <div className='form-row'>
+                            <label>Sicil No:</label>
+                            <input type="text" placeholder='sicil no' />
+                        </div>
+                        <div className='form-row'>
+                            <label>Şifre:</label>
+                            <input type="password" placeholder='şifre' />
+                        </div>
+                        <div className='form-row'>
+                            <label>Montaj No:</label>
+                            <input type="text" placeholder='montaj no' />
+                        </div>
+                        <div className='form-row-date-shift'>
+                            <DateShift/>
+                        </div>
 
-                <div className='form-container'>
-                    <div className='section'>
-                        <label>Terminal Listesi:</label>
-                            <select className='select-terminal-list'>
-                                {veri.map(({termName}) => (
-                                <option key={veri.termId}>{termName}</option>))}
-                            </select>
-                    </div>
-                    
-                    <div className='section'>
-                        <label>Sicil No:</label>
-                        <input type="text" placeholder='sicil no' />
-                    </div>
-                    <div className='section'>
-                        <label>Şifre:</label>
-                        <input type="password" placeholder='şifre' />
-                    </div>
-                    <div className='section'>
-                        <label>Montaj No:</label>
-                        <input type="text" placeholder='montaj no' />
-                    </div>
-                    <div className='section section-date-shift'>
-                        <DateShift/>
-                    </div>
-
-                    <div className='section section-buttons'>
-                        <Button
-                        className="giris"
-                        onClick={handleLoginClick}
-                        text={"Giriş Yap"}
-                        />
-                        <Button
-                        className="kapat"
-                        onClick={handleCloseClick}
-                        text={"Kapat"}
-                        />
+                        <div className='form-row button-grid'>
+                            <Button
+                            className="giris"
+                            onClick={handleLoginClick}
+                            text={"Giriş Yap"}
+                            />
+                            <Button
+                            className="kapat"
+                            onClick={handleCloseClick}
+                            text={"Kapat"}
+                            />
+                        </div>
                     </div>
                 </div>
-                <div className='container-keyboard'>
+                <div className='keyboard-grid'>
                     <Keyboard/>
                 </div>
             </div>
