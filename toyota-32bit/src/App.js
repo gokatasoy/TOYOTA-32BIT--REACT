398c228bb3435ab55b50e7c60239f25d122318a2
Gökberk ATASOY <gokatasoycs@gmail.com>
2023-05-05 10:11:53 +0300

    Add DefectEntryPopup.js and it's css styles

--- toyota-32bit/src/App.js
+++ toyota-32bit/src/App.js
@@ -5,17 +5,19 @@ import DefectPage from './pages/DefectPage/DefectPage';
 import DefectList from "./pages/DefectList/DefectList"
 import BigFont from "./pages/BigFont/BigFont"
 import { Routes, Route} from 'react-router-dom';
+import DefectEntryPopup from './components/defect-entry-popup/DefectEntryPopup';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
+      {/* <Routes>
         <Route path='/' element={<TerminalList/>} />
         <Route path='/terminal-login' element={<TerminalLogin/>} />
         <Route path='/defect-page' element={<DefectPage/>} />
         <Route path='/big-font' element={<BigFont/>} />
         <Route path='/defect-list' element={<DefectList/>} />
-      </Routes>
+      </Routes> */}
+      <DefectEntryPopup/>
     </div>
   );
 }
---
+++ toyota-32bit/src/components/defect-entry-popup/DefectEntryPopup.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import "./defectEntryPopup.css"
+import Keyboard from '../keyboard/Keyboard'
+
+function DefectEntryPopup() {
+    return (
+        <div className='defect-entry-popup-grid'>
+            <div className='defect-entry-popup-container'>
+                <div className='row'>
+                    <div className='terminal-name'><h1>Terminal Name</h1></div>
+                    <div className='common-defect'>SIK GELEN HATA</div>
+                </div>
+                <div className='row'>
+                    <div className='form-container'>
+                        <div className='form-row'>
+                            <label>Hata Sorumlusu</label>
+                            <input type='text' />
+                        </div>
+                        <div className='form-row'>
+                            <label>Hata Sınıfı</label>
+                            <input type='text' />
+                        </div>
+                        <div className='form-row'>
+                            <label>Exit Department</label>
+                            <input type='text' />
+                        </div>
+                        <div className='form-row'>
+                            <label>Açıklama</label>
+                            <input type='text' />
+                        </div>
+                        <div className='form-row'>
+                            <label>Yapılan İşlem</label>
+                            <input type='text' />
+                        </div>
+                        <div className='form-row'>
+                            <label>Alt Sorumlu</label>
+                            <input type='text' />
+                        </div>
+                        <div className='form-row'>
+                            <label>RDD</label>
+                            <input type='text' />
+                        </div>
+                        <div className='form-row button-grid'>
+                            <button className='save'>KAYDET</button>
+                            <button className='close'>İPTAL</button>
+                        </div>
+                    </div>
+                </div>
+                <div className='keyboard-grid'>
+                    <Keyboard />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DefectEntryPopup
