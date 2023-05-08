786bfe3cafe0077cebcc6146f6ea753517671284
Gökberk ATASOY <gokatasoycs@gmail.com>
2023-05-08 18:18:16 +0300

    Edit TerminalList css styles.

--- toyota-32bit/src/pages/TerminalList/TerminalList.js
+++ toyota-32bit/src/pages/TerminalList/TerminalList.js
@@ -1,7 +1,7 @@
 import './terminalList.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 // import { BrowserRouter as Router } from 'react-router-dom';
 
 function TerminalList() {
@@ -9,12 +9,12 @@ function TerminalList() {
     let navigate = useNavigate();
 
     useEffect(() => {
-    axios.get("./terminals.json")
-        .then(res => setVeri(res.data))
-        .catch(error => {
-        console.log(error);
-        setVeri("An error occurred while fetching data");
-        });
+        axios.get("./terminals.json")
+            .then(res => setVeri(res.data))
+            .catch(error => {
+                console.log(error);
+                setVeri("An error occurred while fetching data");
+            });
     }, []);
 
     const handleTerminalClick = () => {
@@ -22,34 +22,34 @@ function TerminalList() {
     }
 
     return (
-    <div className="terminal-list">
-        <div className='terminal-list-headers'>
-            <div className='main-header-h1'>TÜM TERMİNALLER</div>
-            <div className='container'>
-                <div className='section-header-h2'>BÖLÜM BAZINDA</div>
-                <div className='filter-header-h2'>FİLTRE BAZINDA</div>
-            </div>
-        </div>
-        
-        {veri.map(({ depName,shopCode, filterBaseds }) => (
-        <div key={depName}>
-            <ul>
+        <div className="terminal-list-grid">
+            <div className='terminal-list-header-container'>
+                <div className='main-header-h1'>TÜM TERMİNALLER</div>
                 <div className='container'>
-                    <div className='section-side'>
-                        <h2 className='shop-code-title' >({shopCode})</h2>  
-                        <h2 >{depName}</h2>  
+                    <div className='section-header-h2'>BÖLÜM BAZINDA</div>
+                    <div className='filter-header-h2'>FİLTRE BAZINDA</div>
                 </div>
-                    {filterBaseds.map(({ filterCode, linkCount }) => (
-                    <li className='filter-side parent-box' key={filterCode} onClick={handleTerminalClick}>
-                        {filterCode}
-                        <div className='child-box'>{linkCount   }</div>
-                    </li>
-                    ))}
+            </div>
+
+            {veri.map(({ depName, shopCode, filterBaseds }) => (
+                <div key={depName}>
+                    <ul>
+                        <div className='container'>
+                            <div className='section-side'>
+                                <h2 className='shop-code-title' >({shopCode})</h2>
+                                <h2 >{depName}</h2>
+                            </div>
+                            {filterBaseds.map(({ filterCode, linkCount }) => (
+                                <li className='filter-side parent-box' key={filterCode} onClick={handleTerminalClick}>
+                                    {filterCode}
+                                    <div className='child-box'>{linkCount}</div>
+                                </li>
+                            ))}
+                        </div>
+                    </ul>
                 </div>
-            </ul>
+            ))}
         </div>
-        ))}
-    </div>
     );
 }
 
