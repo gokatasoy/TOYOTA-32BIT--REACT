a4f2f09417f28874806a3b62394d3aab405868c2
Gökberk ATASOY <gokatasoycs@gmail.com>
2023-05-08 11:02:57 +0300

    Add mouse pointer to DefectedCar.js

--- toyota-32bit/src/components/defected-car/DefectedCar.js
+++ toyota-32bit/src/components/defected-car/DefectedCar.js
@@ -4,12 +4,36 @@ import Box from '../box/Box';
 import dummyCar from '../../dummy-car.jpeg';
 import DefectionSelectBox from '../defection-selectbox/DefectionSelectBox';
 import "./defectedCar.css"
+import cursor from "../../cursor5.png";
 
 function DefectedCar() {
     const [jsonData, setJsonData] = useState('./picture-data.json');
     const [boxesData, setBoxesData] = useState([]);
     const [carImage, setCarImage] = useState(dummyCar);
     const [showDefectionSelectBox, setShowDefectionSelectBox] = useState(false);
+
+    const [x, setX] = useState(null);
+    const [y, setY] = useState(null);
+
+    const handlePointerClick = (event) => {
+        //WE HAVE CREATED A DIV USING CREATEELEMENT METHOD AND AFTER THAT WE NAMED IT POINTER. AND WE GAVE IT A CLASSNAME.
+        // IT IS POSITIONED AT THE LOCATION OF THE USER'S CLICK.
+        // AND WITH THE APPENDCHILD METHOD WE HAVE ADDED THE NEW DIV UNDER THE PARENT ELEMENT
+        const pointer = document.createElement("div")
+        pointer.className = "pointer"
+        pointer.style.left = `${event.clientX}px`
+        pointer.style.top = `${event.clientY}px`
+        pointer.style.cursor = `url(${cursor}) 16 16, auto`;
+        document.body.appendChild(pointer)
+
+        // THIS FUNCTION CALCULATES THE COORDINATESOF THE USER'S CLICK ACCORDING TO TOP-LEFT CORNER
+        const rect = event.target.getBoundingClientRect();
+        const xCoord = event.clientX - rect.left;
+        const yCoord = event.clientY - rect.top;
+
+        setX(xCoord);
+        setY(yCoord);
+    }
 
     useEffect(() => {
         axios.get(jsonData)
@@ -18,10 +42,10 @@ function DefectedCar() {
     }, [jsonData]);
 
     const handleBoxClick = (picAddress) => {
-        if(jsonData==="./picture-data.json"){
+        if (jsonData === "./picture-data.json") {
             setCarImage(picAddress);
             setJsonData(jsonData === './picture-data.json' ? './picture-data2.json' : './picture-data.json');
-        }else if(jsonData==="./picture-data2.json"){
+        } else if (jsonData === "./picture-data2.json") {
             setShowDefectionSelectBox(true)
         }
     };
@@ -29,7 +53,13 @@ function DefectedCar() {
     return (
         <div className='defected-car-grid'>
             <div className="image-container">
-                <img src={carImage} alt="dummy-car" />
+                <img src={carImage} alt="dummy-car" onClick={handlePointerClick} />
+                {x !== null && y !== null && (
+                    <div>
+                        <p>X: {x}</p>
+                        <p>Y: {y}</p>
+                    </div>
+                )}
                 <div className="box-container">
                     {boxesData.map(({ defectButtonRecords }) => (
                         <div key={defectButtonRecords}>
@@ -39,13 +69,13 @@ function DefectedCar() {
                                         key={boxData.buttonId}
                                         boxData={boxData}
                                         handleBoxClick={handleBoxClick}
-                                    />                                  
+                                    />
                                 </div>
                             ))}
                         </div>
                     ))}
                     <div>
-                        {showDefectionSelectBox && <DefectionSelectBox />}    
+                        {showDefectionSelectBox && <DefectionSelectBox />}
                     </div>
                 </div>
             </div>
