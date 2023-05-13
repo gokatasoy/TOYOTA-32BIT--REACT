cf9ee1f6e7c5064a7f65ebc65df4c7bdd9aa77e1
Gökberk ATASOY <gokatasoycs@gmail.com>
2023-05-13 13:26:23 +0300

    Add Keyboard functions to DefectEntryPopup.js

--- toyota-32bit/src/components/defect-entry-popup/DefectEntryPopup.js
+++ toyota-32bit/src/components/defect-entry-popup/DefectEntryPopup.js
@@ -4,10 +4,38 @@ import Keyboard from '../keyboard/Keyboard'
 import { useState } from 'react';
 
 function DefectEntryPopup({ onClose }) {
-    const [inputValue, setInputValue] = useState("");
+    const [inputValues, setInputValues] = useState({
+        hataSorumlusu: "",
+        hataSinifi: "",
+        exitDepartment: "",
+        aciklama: "",
+        islem: "",
+        altSorumlu: "",
+        rdd: "",
+    });
+    const [isInputFocused, setIsInputFocused] = useState(false);
+    const [focusedInput, setFocusedInput] = useState("");
 
+    // KEYBOARD FUNCTION
     const handleKeyPressed = (key) => {
-        setInputValue(inputValue + key);
+        if (isInputFocused && focusedInput !== "") {
+            if (key === 'delete') {
+                setInputValues(prevValues => ({
+                    ...prevValues,
+                    [focusedInput]: prevValues[focusedInput].slice(0, -1)
+                }));
+            } else {
+                setInputValues(prevValues => ({
+                    ...prevValues,
+                    [focusedInput]: prevValues[focusedInput] + key
+                }));
+            }
+        }
+    };
+
+    const handleInputFocused = (inputName) => {
+        setIsInputFocused(true);
+        setFocusedInput(inputName);
     };
 
     const handlePopupClose = () => {
@@ -24,31 +52,66 @@ function DefectEntryPopup({ onClose }) {
                     <div className='form-container'>
                         <div className='form-row'>
                             <label>Hata Sorumlusu</label>
-                            <input type='text' />
+                            <input
+                                type='text'
+                                placeholder='hata sorumlusu'
+                                value={inputValues.sicilNo}
+                                onFocus={() => handleInputFocused("hataSorumlusu")}
+                            />
                         </div>
                         <div className='form-row'>
                             <label>Hata Sınıfı</label>
-                            <input type='text' value={inputValue} />
+                            <input
+                                type='text'
+                                placeholder='hata sınıfı'
+                                value={inputValues.hataSinifi}
+                                onFocus={() => handleInputFocused("hataSinifi")}
+                            />
                         </div>
                         <div className='form-row'>
                             <label>Exit Department</label>
-                            <input type='text' />
+                            <input
+                                type='text'
+                                placeholder='exit department'
+                                value={inputValues.exitDepartment}
+                                onFocus={() => handleInputFocused("exitDepartment")}
+                            />
                         </div>
                         <div className='form-row'>
                             <label>Açıklama</label>
-                            <input type='text' />
+                            <input
+                                type='text'
+                                placeholder='açıklama'
+                                value={inputValues.aciklama}
+                                onFocus={() => handleInputFocused("aciklama")}
+                            />
                         </div>
                         <div className='form-row'>
                             <label>Yapılan İşlem</label>
-                            <input type='text' />
+                            <input
+                                type='text'
+                                placeholder='yapılan işlem'
+                                value={inputValues.islem}
+                                onFocus={() => handleInputFocused("islem")}
+                            />
                         </div>
                         <div className='form-row'>
                             <label>Alt Sorumlu</label>
-                            <input type='text' />
+                            <input
+                                type='text'
+                                placeholder='alt sorumlu'
+                                value={inputValues.altSorumlu}
+                                onFocus={() => handleInputFocused("altSorumlu")}
+                            />
                         </div>
                         <div className='form-row'>
                             <label>RDD</label>
-                            <input type='text' />
+                            <input
+                                type='text'
+                                placeholder='rdd'
+                                value={inputValues.rdd}
+                                onFocus={() => handleInputFocused("rdd")}
+                            />
                         </div>
                         <div className='form-row button-grid'>
                             <button className='save'>KAYDET</button>
@@ -57,7 +120,7 @@ function DefectEntryPopup({ onClose }) {
                     </div>
                 </div>
                 <div className='keyboard-component-grid'>
-                    <Keyboard onKeyPressed={handleKeyPressed}/>
+                    <Keyboard onKeyPressed={handleKeyPressed} />
                 </div>
             </div>
         </div>
