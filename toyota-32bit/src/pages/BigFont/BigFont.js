75130fb9c8fc2a178d41590d2cd60817b7a0dfcc
Gökberk ATASOY <gokatasoycs@gmail.com>
2023-05-16 18:29:38 +0300

    Add AFK background to BigFont.js

--- toyota-32bit/src/pages/BigFont/BigFont.js
+++ toyota-32bit/src/pages/BigFont/BigFont.js
@@ -7,58 +7,86 @@ import DefectEntryContainer from '../../components/defect-entry-container/Defect
 
 const BigFont = () => {
 
+    // FETCH DATA
+    const [vehicleData, setVehicleData] = useState([]);
+    const [defectData, setDefectData] = useState([]);
+
+    useEffect(() => {
+        axios.get("./vehicle-data.json")
+            .then(res => setVehicleData(res.data))
+            .catch(error => {
+                console.log(error);
+                setVehicleData("An error occured while fetching data");
+            });
+
+        axios.get("./defect-data.json")
+            .then(res => setDefectData(res.data))
+            .catch(error => {
+                console.log(error);
+                setVehicleData("An error occured while fetching data.")
+            });
+    }, [])
+
+    // AFK ALERT
+    const [isUserActive, setIsUserActive] = useState(false)
+
+    useEffect(() => {
+        let activityTimer;
+
+        const resetTimer = () => {
+            clearTimeout(activityTimer);
+            activityTimer = setTimeout(() => {
+                setIsUserActive(false);
+            }, 3000);
+        };
+
+        const handleUserActivity = () => {
+            setIsUserActive(true);
+            resetTimer();
+        };
+
+        window.addEventListener('mousemove', handleUserActivity);
+        window.addEventListener('keydown', handleUserActivity);
+
+        return () => {
+            window.removeEventListener('mousemove', handleUserActivity);
+            window.removeEventListener('keydown', handleUserActivity);
+            clearTimeout(activityTimer);
+        };
+    }, []);
 
 
-const [vehicleData, setVehicleData] = useState([]);
-const [defectData, setDefectData] = useState([]);
+    return (
+        <div className={`BigFont-grid ${isUserActive ? 'active' : 'inactive'}`}>
+            <div className='defect-page-header'>
+                <DefectPageHeader />
+            </div>
 
-useEffect(() => {
-    axios.get("./vehicle-data.json")
-        .then(res => setVehicleData(res.data))
-        .catch(error => {
-            console.log(error);
-            setVehicleData("An error occured while fetching data");
-        });
-
-    axios.get("./defect-data.json")
-        .then(res => setDefectData(res.data))
-        .catch(error => {
-            console.log(error);
-            setVehicleData("An error occured while fetching data.")
-        });
-}, [])
-
-return (
-    <div className='BigFont-grid'>
-        <div className='defect-page-header'>
-            <DefectPageHeader />
-        </div>
-
-        <div className='BigFont-grid-container'>
-            <div className='BigFont-row vehicle'>
-                {vehicleData.map(({ modelName, modelId }) => (
-                    <div className='vehicle-data'>
-                        <div>{modelName}</div>
-                        <div>{modelId}</div>
+            <div className='BigFont-grid-container'>
+                <div className='BigFont-row vehicle'>
+                    {vehicleData.map(({ modelName, modelId }) => (
+                        <div className='vehicle-data'>
+                            <div>{modelName}</div>
+                            <div>{modelId}</div>
+                        </div>
+                    ))}
+                    <div className='defect-entry-container'>
+                        <DefectEntryContainer />
                     </div>
-                ))}
-                <div className='defect-entry-container'>
-                    <DefectEntryContainer />
                 </div>
-            </div>
-            <div className='BigFont-row defect'>
-                {defectData.map(({ partName, defectName }) => (
-                    <div className='defect-data'>
-                        <div>{partName}</div>
-                        <div style={{ margin: "0 .3rem" }}>-</div>
-                        <div>{defectName}</div>
-                    </div>
-                ))}
-            </div>
+                <div className='BigFont-row defect'>
+                    {defectData.map(({ partName, defectName }) => (
+                        <div className='defect-data'>
+                            <div>{partName}</div>
+                            <div style={{ margin: "0 .3rem" }}>-</div>
+                            <div>{defectName}</div>
+                        </div>
+                    ))}
+                </div>
 
+            </div>
         </div>
-    </div>
-)
+    )
 }
 
 export default BigFont
