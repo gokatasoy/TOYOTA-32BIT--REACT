9cf199744c7918d2d13db14d5c45c3dd8b2bc95e
Gökberk ATASOY <gokatasoycs@gmail.com>
2023-06-03 18:21:20 +0300

    Add "En" and "Tr" translations to Keyboard component

--- toyota-32bit/src/components/keyboard/Keyboard.js
+++ toyota-32bit/src/components/keyboard/Keyboard.js
@@ -1,41 +1,45 @@
 import React, { useState, useContext } from "react";
 import "./keyboard.css"
 import { KeyboardContext } from '../../context/KeyboardContext'
+import { useTranslation } from 'react-i18next'
+
 
 function Keyboard(props) {
-    const {handleKeyPressed} = useContext(KeyboardContext)
-    
+    const { t } = useTranslation();
+
+    const { handleKeyPressed } = useContext(KeyboardContext)
+
     const [shift, setShift] = useState(false);
 
     const keys = [
         [
-            { className:"keys", label: "-", value: "-" },
-            { className:"keys", label: "1", value: "1" },
-            { className:"keys", label: "2", value: "2" },
-            { className:"keys", label: "3", value: "3" },
-            { className:"keys", label: "4", value: "4" },
-            { className:"keys", label: "5", value: "5" },
-            { className:"keys", label: "6", value: "6" },
-            { className:"keys", label: "7", value: "7" },
-            { className:"keys", label: "8", value: "8" },
-            { className:"keys", label: "9", value: "9" },
-            { className:"keys", label: "0", value: "0" },
-            { className:"keys delete-key", label: "Delete", value: "delete" },
+            { className: "keys", label: t("keyboard.-"), value: t("keyboard.-") },
+            { className: "keys", label: t("keyboard.1"), value: t("keyboard.1") },
+            { className: "keys", label: t("keyboard.2"), value: t("keyboard.2") },
+            { className: "keys", label: t("keyboard.3"), value: t("keyboard.3") },
+            { className: "keys", label: t("keyboard.4"), value: t("keyboard.4") },
+            { className: "keys", label: t("keyboard.5"), value: t("keyboard.5") },
+            { className: "keys", label: t("keyboard.6"), value: t("keyboard.6") },
+            { className: "keys", label: t("keyboard.7"), value: t("keyboard.7") },
+            { className: "keys", label: t("keyboard.8"), value: t("keyboard.8") },
+            { className: "keys", label: t("keyboard.9"), value: t("keyboard.9") },
+            { className: "keys", label: t("keyboard.0"), value: t("keyboard.0") },
+            { className: "keys delete-key", label: "Delete", value: "delete" },
         ],
         [
-            { className:"keys", label: "@", value: "@" },
-            { className:"keys", label: "q", value: "q" },
-            { className:"keys", label: "w", value: "w" },
-            { className:"keys", label: "e", value: "e" },
-            { className:"keys", label: "r", value: "r" },
-            { className:"keys", label: "t", value: "t" },
-            { className:"keys", label: "y", value: "y" },
-            { className:"keys", label: "u", value: "u" },
-            { className:"keys", label: "i", value: "i" },
-            { className:"keys", label: "o", value: "o" },
-            { className:"keys", label: "p", value: "p" },
-            { className:"keys", label: "ğ", value: "ğ" },
-            { className:"keys", label: "ü", value: "ü" },
+            { className: "keys", label: t("keyboard.@"), value: t("keyboard.@") },
+            { className: "keys", label: t("keyboard.q"), value: t("keyboard.q") },
+            { className: "keys", label: t("keyboard.w"), value: t("keyboard.w") },
+            { className: "keys", label: t("keyboard.e"), value: t("keyboard.e") },
+            { className: "keys", label: t("keyboard.r"), value: t("keyboard.r") },
+            { className: "keys", label: t("keyboard.t"), value: t("keyboard.t") },
+            { className: "keys", label: t("keyboard.y"), value: t("keyboard.y") },
+            { className: "keys", label: t("keyboard.u"), value: t("keyboard.u") },
+            { className: "keys", label: t("keyboard.i"), value: t("keyboard.i") },
+            { className: "keys", label: t("keyboard.o"), value: t("keyboard.o") },
+            { className: "keys", label: t("keyboard.p"), value: t("keyboard.p") },
+            { className: "keys", label: t("keyboard.double-dot"), value: t("keyboard.double-dot") },
+            { className: "keys", label: t("keyboard.;"), value: t("keyboard.;") },
         ],
         [
             {
@@ -43,32 +47,32 @@ function Keyboard(props) {
                 value: "shift",
                 className: `keys shift-key ${shift ? "active" : ""}`,
             },
-            { className:"keys", label: "a", value: "a" },
-            { className:"keys", label: "s", value: "s" },
-            { className:"keys", label: "d", value: "d" },
-            { className:"keys", label: "f", value: "f" },
-            { className:"keys", label: "g", value: "g" },
-            { className:"keys", label: "h", value: "h" },
-            { className:"keys", label: "j", value: "j" },
-            { className:"keys", label: "k", value: "k" },
-            { className:"keys", label: "l", value: "l" },
-            { className:"keys", label: "ş", value: "ş" },
-            { className:"keys", label: "i", value: "i" },
+            { className: "keys", label: t("keyboard.a"), value: t("keyboard.a") },
+            { className: "keys", label: t("keyboard.s"), value: t("keyboard.s") },
+            { className: "keys", label: t("keyboard.d"), value: t("keyboard.d") },
+            { className: "keys", label: t("keyboard.f"), value: t("keyboard.f") },
+            { className: "keys", label: t("keyboard.g"), value: t("keyboard.g") },
+            { className: "keys", label: t("keyboard.h"), value: t("keyboard.h") },
+            { className: "keys", label: t("keyboard.j"), value: t("keyboard.j") },
+            { className: "keys", label: t("keyboard.k"), value: t("keyboard.k") },
+            { className: "keys", label: t("keyboard.l"), value: t("keyboard.l") },
+            { className: "keys", label: t("keyboard.?"), value: t("keyboard.?") },
+            { className: "keys", label: t("keyboard.!"), value: t("keyboard.!") },
         ],
         [
-            { className:"keys", label: ".", value: "." },
-            { className:"keys", label: ",", value: "," },
-            { className:"keys", label: "z", value: "z" },
-            { className:"keys", label: "x", value: "x" },
-            { className:"keys", label: "c", value: "c" },
-            { className:"keys", label: "v", value: "v" },
-            { className:"keys", label: "b", value: "b" },
-            { className:"keys", label: "n", value: "n" },
-            { className:"keys", label: "m", value: "m" },
-            { className:"keys", label: "ö", value: "ö" },
-            { className:"keys", label: "ç", value: "ç" },
-            { className:"keys enter-key", label: "Enter", value: "enter" },
-            
+            { className: "keys", label: t("keyboard.."), value: t("keyboard..") },
+            { className: "keys", label: t("keyboard.,"), value: t("keyboard.,") },
+            { className: "keys", label: t("keyboard.z"), value: t("keyboard.z") },
+            { className: "keys", label: t("keyboard.x"), value: t("keyboard.x") },
+            { className: "keys", label: t("keyboard.c"), value: t("keyboard.c") },
+            { className: "keys", label: t("keyboard.v"), value: t("keyboard.v") },
+            { className: "keys", label: t("keyboard.b"), value: t("keyboard.b") },
+            { className: "keys", label: t("keyboard.n"), value: t("keyboard.n") },
+            { className: "keys", label: t("keyboard.m"), value: t("keyboard.m") },
+            { className: "keys", label: t("keyboard.<"), value: t("keyboard.<") },
+            { className: "keys", label: t("keyboard.>"), value: t("keyboard.>") },
+            { className: "keys enter-key", label: "Enter", value: "enter" },
+
         ],
         [
             {
@@ -88,7 +92,7 @@ function Keyboard(props) {
             props.handleKeyPressed(shift ? key.handleKeyPressed() : key);
         }
     };
-    
+
 
     return (
         <div className="keyboard-container">
