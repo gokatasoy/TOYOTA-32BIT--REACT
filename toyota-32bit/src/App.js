ad328a270e0cb8f013fef89521356344ce06e699
Gökberk ATASOY <gokatasoycs@gmail.com>
2023-06-03 10:17:28 +0300

    Add i18 next and the translation jsons

--- toyota-32bit/src/App.js
+++ toyota-32bit/src/App.js
@@ -6,20 +6,24 @@ import DefectList from "./pages/DefectList/DefectList"
 import BigFont from "./pages/BigFont/BigFont"
 import { Routes, Route} from 'react-router-dom';
 import { KeyboardProvider } from './context/KeyboardContext';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 
 function App() {
   return (
-    <KeyboardProvider>
-      <div className="App">
-        <Routes>
-          <Route path='/' element={<TerminalList/>} />
-          <Route path='/terminal-login' element={<TerminalLogin/>} />
-          <Route path='/defect-page' element={<DefectPage/>} />
-          <Route path='/big-font' element={<BigFont/>} />
-          <Route path='/defect-list' element={<DefectList/>} />
-        </Routes>
-      </div>
-    </KeyboardProvider>
+    <I18nextProvider i18n={i18n}>
+      <KeyboardProvider>
+        <div className="App">
+          <Routes>
+            <Route path='/' element={<TerminalList/>} />
+            <Route path='/terminal-login' element={<TerminalLogin/>} />
+            <Route path='/defect-page' element={<DefectPage/>} />
+            <Route path='/big-font' element={<BigFont/>} />
+            <Route path='/defect-list' element={<DefectList/>} />
+          </Routes>
+        </div>
+      </KeyboardProvider>
+    </I18nextProvider>
   );
 }
 
---
+++ toyota-32bit/src/i18n.js
@@ -0,0 +1,28 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+import trTranslation from "./language/tr/translation.json";
+import enTranslation from "./language/en/translation.json";
+import jpTranslation from "./language/jp/translation.json";
+
+i18n
+    .use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources: {
+            tr: { translation: trTranslation },
+            en: { translation: enTranslation },
+            jp: { translation: jpTranslation },
+        },
+        fallbackLng: "en",
+        detection: {
+            order: ["navigator"],
+        },
+        interpolation: {
+            escapeValue: false, 
+        },
+    });
+
+export default i18n;
