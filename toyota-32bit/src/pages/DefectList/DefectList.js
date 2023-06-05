9b7472d9dcf18e88fa2e57ff6f811fc0336a398c
Gökberk ATASOY <gokatasoycs@gmail.com>
2023-06-05 16:11:23 +0300

    Add MUI to DefectList.js (V1)

--- toyota-32bit/src/pages/DefectList/DefectList.js
+++ toyota-32bit/src/pages/DefectList/DefectList.js
@@ -1,15 +1,15 @@
 import './defectList.css';
-import { useState, useEffect } from "react";
-import axios from "axios";
-import NrReason from "../../components/nr-reason/NrReason"
+import React, { useState, useEffect } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import axios from 'axios';
+import NrReason from '../../components/nr-reason/NrReason';
 import BottomGrid from "../../components/bottom-grid/BottomGrid"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSave } from '@fortawesome/free-regular-svg-icons';
-import { faTrash, faPen } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSave, faTrash, faPen } from '@fortawesome/free-solid-svg-icons';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
+const DefectList = () => {
   const [veri, setVeri] = useState([]);
 
   const handleSave = () => {
@@ -19,11 +19,11 @@ function App() {
       autoClose: 1000,
       hideProgressBar: true,
     });
-  }
+  };
 
   useEffect(() => {
     axios
-      .get("./HataListesi.json")
+      .get('./HataListesi.json')
       .then((res) => setVeri(res.data))
       .catch((error) => {
         console.log(error);
@@ -31,77 +31,93 @@ function App() {
       });
   }, []);
 
+  const columns = [
+    { field: 'depCode', headerName: 'Bildiren', width: 70 },
+    { field: 'bodyNo', headerName: 'Body', width: 70 },
+    { field: 'assyNo', headerName: 'Assy', width: 70 },
+    { field: 'vinNo', headerName: 'Vin No', width: 200 },
+    { field: 'colorExtCode', headerName: 'Renk', width: 70 },
+    { field: 'modelCode', headerName: 'Mdl', width: 70 },
+    { field: 'localId', headerName: 'Sicil', width: 70 },
+    { field: 'partName', headerName: 'Parça', width: 200 },
+    { field: 'spot', headerName: 'Spot', width: 60 },
+    { field: 'gun', headerName: 'Gun', width: 70 },
+    { field: 'arcgun', headerName: 'ArcGun', width: 70 },
+    { field: 'hata', headerName: 'Hata', width: 70 },
+    { field: 'rank', headerName: 'Rank', width: 70 },
+    { field: 'formattedDefectHour', headerName: 'Saat', width: 100 },
+    { field: 'defectCode', headerName: 'Hata Türü', width: 100 },
+    { field: 'defrespName', headerName: 'Hata Sor', width: 100 },
+    { field: 'altSorumlu', headerName: 'Alt Sorumlu', width: 100 },
+    { field: 'nrReason', headerName: 'NR REASON', width: 130, renderCell: () => <NrReason /> },
+    {
+      field: 'kaydet', headerName: 'Kaydet', width: 100, renderCell: () => (
+        <FontAwesomeIcon
+          className="saveIcon"
+          icon={faSave}
+          onClick={handleSave}
+        />
+      )
+    },
+    {
+      field: 'islem', headerName: 'İşlem', width: 100, renderCell: () => (
+        <div>
+          <FontAwesomeIcon className="editIcon" icon={faPen} />
+          <FontAwesomeIcon className="deleteIcon" icon={faTrash} />
+        </div>
+      )
+    },
+  ];
+
+  const columnProperties = {
+    headerClassName: 'headerStyle',
+    cellClassName: 'cellStyle'
+  };
+
+  const columnsWithStyles = columns.map(column => ({ ...column, ...columnProperties }));
+
+  const rows = veri.flatMap(({ data }) =>
+    data.flatMap(({ defectList }) =>
+      defectList.map(
+        ({
+          depCode,
+          bodyNo,
+          assyNo,
+          vinNo,
+          colorExtCode,
+          modelCode,
+          localId,
+          partName,
+          formattedDefectHour,
+          defectCode,
+          defrespName,
+        }) => ({
+          id: depCode,
+          depCode,
+          bodyNo,
+          assyNo,
+          vinNo,
+          colorExtCode,
+          modelCode,
+          localId,
+          partName,
+          formattedDefectHour,
+          defectCode,
+          defrespName,
+        })
+      )
+    )
+  );
+
   return (
-    <div className="App">
-      <table>
-        <thead className='tableHead'>
-          <tr>
-            <th>Bildiren</th>
-            <th>Body</th>
-            <th>Assy</th>
-            <th>Vin No </th>
-            <th>Renk</th>
-            <th>Mdl</th>
-            <th>Sicil</th>
-            <th>Parça</th>
-            <th>Spot</th>
-            <th>Gun</th>
-            <th>ArcGun</th>
-            <th>Hata</th>
-            <th>Rank</th>
-            <th>Saat</th>
-            <th>Hata Türü</th>
-            <th>Hata Sor</th>
-            <th>Alt Sorumlu</th>
-            <th>NR REASON</th>
-            <th>Kaydet</th>
-            <th>Düzenle / Sil</th>
-          </tr>
-        </thead>
-        <tbody className='tableBody'>
-          {veri.map(({ data }) =>
-            data.map(({ defectList }) =>
-              defectList.map(({ depCode, bodyNo, assyNo, vinNo, colorExtCode, modelCode, localId, partName, formattedDefectHour, defectCode, defrespName }) =>
-                <tr>
-                  <td>{depCode}</td>
-                  <td>{bodyNo}</td>
-                  <td>{assyNo}</td>
-                  <td>{vinNo}</td>
-                  <td>{colorExtCode}</td>
-                  <td>{modelCode}</td>
-                  <td>{localId}</td>
-                  <td className='partName'>{partName}</td>
-                  <td>{ }</td>
-                  <td>{ }</td>
-                  <td>{ }</td>
-                  <td>{ }</td>
-                  <td>{ }</td>
-                  <td>{formattedDefectHour}</td>
-                  <td>{defectCode}</td>
-                  <td>{defrespName}</td>
-                  <td>{ }</td>
-                  <td>
-                    <NrReason />
-                  </td>
-                  <td>
-                    <FontAwesomeIcon className='saveIcon' icon={faSave} onClick={handleSave} />
-                  </td>
-                  <td>
-                    <FontAwesomeIcon className='editIcon' icon={faPen} />
-                    <FontAwesomeIcon className='deleteIcon' icon={faTrash} />
-                  </td>
-                </tr>
-              )
-            )
-          )}
-        </tbody>
-      </table>
+    <div style={{ height: "%100", width: '100%' }}>
+      <DataGrid columns={columnsWithStyles} rows={rows} />
       <ToastContainer />
       <div className='bottom-grid'>
         <BottomGrid />
       </div>
     </div>
   );
-}
+};
 
-export default App;
+export default DefectList;
