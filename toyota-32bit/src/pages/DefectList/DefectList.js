6b601ec571c0f034414eb80d292e3a63338dbc15
Gökberk ATASOY <gokatasoycs@gmail.com>
2023-06-06 17:02:58 +0300

    MUI DataGrid component (V2)

--- toyota-32bit/src/pages/DefectList/DefectList.js
+++ toyota-32bit/src/pages/DefectList/DefectList.js
@@ -31,24 +31,33 @@ const DefectList = () => {
       });
   }, []);
 
+  //////////////////////////////////////////////////////////////////////////////////////////////////
   const columns = [
-    { field: 'depCode', headerName: 'Bildiren', width: 70 },
-    { field: 'bodyNo', headerName: 'Body', width: 70 },
-    { field: 'assyNo', headerName: 'Assy', width: 70 },
+    { field: 'depCode', headerName: 'Bildiren', width: 85 },
+    { field: 'bodyNo', headerName: 'Body', width: 65 },
+    { field: 'assyNo', headerName: 'Assy', width: 6 },
     { field: 'vinNo', headerName: 'Vin No', width: 200 },
     { field: 'colorExtCode', headerName: 'Renk', width: 70 },
     { field: 'modelCode', headerName: 'Mdl', width: 70 },
     { field: 'localId', headerName: 'Sicil', width: 70 },
-    { field: 'partName', headerName: 'Parça', width: 200 },
+
+    // ADDITIONAL CLASSNAME FOR PARTNAME
+    {
+      field: 'partName', headerName: 'Parça', width: 200, renderCell: (params) => (
+        <div className={`cellStyle ${params.field === 'partName' ? 'partNameStyle' : ''}`}>
+          {params.value}
+        </div>
+      ),
+    },
     { field: 'spot', headerName: 'Spot', width: 60 },
-    { field: 'gun', headerName: 'Gun', width: 70 },
-    { field: 'arcgun', headerName: 'ArcGun', width: 70 },
-    { field: 'hata', headerName: 'Hata', width: 70 },
-    { field: 'rank', headerName: 'Rank', width: 70 },
+    { field: 'gun', headerName: 'Gun', width: 60 },
+    { field: 'arcgun', headerName: 'ArcGun', width: 80 },
+    { field: 'hata', headerName: 'Hata', width: 60 },
+    { field: 'rank', headerName: 'Rank', width: 60 },
     { field: 'formattedDefectHour', headerName: 'Saat', width: 100 },
     { field: 'defectCode', headerName: 'Hata Türü', width: 100 },
     { field: 'defrespName', headerName: 'Hata Sor', width: 100 },
-    { field: 'altSorumlu', headerName: 'Alt Sorumlu', width: 100 },
+    { field: 'altSorumlu', headerName: 'Alt Sorumlu', width: 110 },
     { field: 'nrReason', headerName: 'NR REASON', width: 130, renderCell: () => <NrReason /> },
     {
       field: 'kaydet', headerName: 'Kaydet', width: 100, renderCell: () => (
@@ -60,7 +69,7 @@ const DefectList = () => {
       )
     },
     {
-      field: 'islem', headerName: 'İşlem', width: 100, renderCell: () => (
+      field: 'islem', headerName: 'İşlem', width: 120, renderCell: () => (
         <div>
           <FontAwesomeIcon className="editIcon" icon={faPen} />
           <FontAwesomeIcon className="deleteIcon" icon={faTrash} />
@@ -69,6 +78,7 @@ const DefectList = () => {
     },
   ];
 
+  // ADD HEADER AND CELL CLASSNAMES
   const columnProperties = {
     headerClassName: 'headerStyle',
     cellClassName: 'cellStyle'
@@ -109,9 +119,13 @@ const DefectList = () => {
     )
   );
 
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+
   return (
     <div style={{ height: "%100", width: '100%' }}>
-      <DataGrid columns={columnsWithStyles} rows={rows} />
+      <div className='data-grid'>
+        <DataGrid columns={columnsWithStyles} rows={rows} />
+      </div>
       <ToastContainer />
       <div className='bottom-grid'>
         <BottomGrid />
